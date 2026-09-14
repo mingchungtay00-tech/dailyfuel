dd8406d2aaf37ca30dbec4635523080246ff8774
mingchungtay00-tech <mingchungtay00@gmail.com>
2026-09-14 21:34:06 +0800

    Add DailyFuel application files

---
+++ app/manifest.js
@@ -0,0 +1,1 @@
+export default function manifest(){return{name:"DailyFuel",short_name:"DailyFuel",start_url:"/",display:"standalone",background_color:"#eef9ff",theme_color:"#dff3ff",icons:[{src:"/icon-192.png",sizes:"192x192",type:"image/png",purpose:"any maskable"},{src:"/icon-512.png",sizes:"512x512",type:"image/png",purpose:"any maskable"}]}}
